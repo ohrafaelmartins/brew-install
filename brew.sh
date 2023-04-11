41fa77631ba913617860d9feaa05588654907186
Rafael Martins <rafaelmartins025@outlook.com>
2023-04-11 16:15:16 -0300

    Create brew.sh

---
+++ brew.sh
@@ -0,0 +1,25 @@
+#!/bin/zsh
+
+# Your script code goes here
+
+brew install --cask iterm2
+brew install --cask spotify
+brew install --cask visual-studio-code
+brew install --cask microsoft-edge
+brew install --cask google-drive
+brew install terraform
+brew install terragrunt
+brew install docker
+brew install awscli
+brew install kubectx
+brew install kubernetes-cli
+brew install --cask pritunl
+brew install terraformer
+brew install --cask dbeaver-community
+brew install telnet
+brew install watch
+brew install helm
+brew install k9s
+brew install --cask meld
+brew install tfsec
+brew install rename
